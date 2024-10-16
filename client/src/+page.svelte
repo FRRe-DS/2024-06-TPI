71de46f226212f50e6582d0afbe7994482e3ddd1
Max <maxcocito@gmail.com>
2024-10-16 10:46:41 -0300

    cambios en el front

---
+++ client/src/+page.svelte
@@ -0,0 +1,15 @@
+
+<script context="module" lang="ts">
+    import { redirect } from '@sveltejs/kit';
+
+    // Redirige a la ruta '/inicio'
+    export const load = () => {
+        throw redirect(302, '/inicio');
+    };
+</script>
+
+<!-- Puedes dejar el contenido vacío si solo necesitas la redirección -->
+<main>
+
+    <h1>redirigiendo....</h1>
+</main>
