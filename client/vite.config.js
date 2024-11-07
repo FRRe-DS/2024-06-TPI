a7cb92c8b5e22301bea9594ebb9229215fa7caff
Beneyto Mateo <mateobeneyto@gmail.com>
2024-11-07 04:21:57 -0300

    ultimo
    e

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		port: 3001, // O el puerto que quieras usar
+		port: 3333, // O el puerto que quieras usar
 		host: true // Esto permite que el servidor sea accesible desde fuera de localhost
 	}
 });
